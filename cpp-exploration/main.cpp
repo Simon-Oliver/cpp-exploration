021d4b724ad4614c99cef024426a24fa73b1e9ef
Simon-Oliver <stauffer.simon@outlook.com>
2020-08-15 07:03:36 +0200

    Initial Commit

---
+++ cpp-exploration/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp-exploration
+//
+//  Created by Simon St. on 15/8/20.
+//  Copyright © 2020 Simon St. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
